57a026b487baaac40024e4fedeedef02bb32dbdd
Jan McSorley <mcsorleyjan@gmail.com>
2023-08-06 12:13:35 -0500

    feat: Add grid to CSS and add files to file system

--- src/dataModel.js
+++ src/dataModel.js
@@ -5,18 +5,50 @@ const getCurrentTraveler = (travelers, id) => {
   
   // ~~~~~~~~~Iteration 1 ~~~~~~~~~~~~~
   
-const getAllTrips = (trips, id, status) => {
+// const getTripInfo = (trips, userID, status) => {
+//     return trips.filter(trip => {
+//         return trip.userID === userID &&
+//         (trip.status === status || status === undefined)
+//     })
+// }
+
+//Travelers dataSet: id = the traveler's ID
+//Trips dataSet: id = Trip#;  userID = traveler#; destinationID = destination#
+//Destinations dataSet id = destination#
+
+const getAllTrips = (trips, userID) => {
     return trips.filter(trip => {
-        return trip.id === id &&
-        (trip.status === status || status === undefined)
+        return trip.userID === userID
     })
 }
+
+const getPastTrips = (trips, userID) => {
+    return trips.filter(trip => {
+        return trip.userID === userID && trip.status === 'approved'
+    })
+}
+
+const getUpcomingTrips = (trips, userID) => {
+    return trips.filter(trip => {
+        return trip.userID === userID && trip.status === 'pending'
+    })
+}
+
+// const getPastTrips = (userID) => {
+
+//     getTripInfo(fetchedData.trips, 50, 'approved')
+// }
+
   
-  const getTotalAnnualSpend = (trips, destinations, id) => {
+//   const getTotalAnnualSpend = (trips, destinations, id, year) => {
+//     const filterdTripsByYear = trips.filter(trip => trip.userID === id && trip.) 
+
+//     }
+    
       // Total amount I have spent on trips this year. This should be calculated from the trips data and include a travel agent’s 10% fee
 
       //look up ID in trips - get the desitnation ID, then look up destination ID in destinations and then do math :-(
-  }
+//   }
   
   // ~~~~~~~~~Iteration 2 ~~~~~~~~~~~~~
   
@@ -25,27 +57,27 @@ const getAllTrips = (trips, id, status) => {
 
   //have a collection of methods that do nothing other than pull the information from the form - getSelectedTripDate; getSelectedDestination, getSelectedNumberOfTravelers - when you want to display a new trip, you will pull these pieces and then stuff them back into the DOM
 
-  const displayNewTrip = () => {
-      //show trip details (date, duration, number of travelers, destination)
-      //show estimated cost with 10% trave; agent's fee
-      //status is pending
-  }
+//   const displayNewTrip = () => {
+//       //show trip details (date, duration, number of travelers, destination)
+//       //show estimated cost with 10% trave; agent's fee
+//       //status is pending
+//   }
 
-  const createNewTrip = (travelerID) => {
-    // select a:
-        // date
-        // duration
-        // number of travelers
-        // choose from a list of destinations
-        // estimated cost (with a 10% travel agent fee) for the trip
+//   const createNewTrip = (travelerID) => {
+//     // select a:
+//         // date
+//         // duration
+//         // number of travelers
+//         // choose from a list of destinations
+//         // estimated cost (with a 10% travel agent fee) for the trip
   
-  }
+//   }
   
   //Iteration 4 - Login
   
-  const getUserLogin = () => {
-      //get value from userName field and password field
-  }
+//   const getUserLogin = () => {
+//       //get value from userName field and password field
+//   }
   
   /*
   
@@ -59,6 +91,9 @@ const getAllTrips = (trips, id, status) => {
 
 export {
     getCurrentTraveler,
-    getAllTrips
+    // getTripInfo
+    getAllTrips,
+    getPastTrips,
+    getUpcomingTrips
 }
 
--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,22 @@
 /* ~~~~~~~ IMPORTS from apiCalls ~~~~~~~*/
 
 import { fetchedData, fetchPromises } from './apiCalls';
+import './css/normalize.css';
 import './css/styles.css';
 import './css/normalize.css';
 import './images/turing-logo.png';
+import './images/road.jpg';
+import './images/ocean.jpg'
 
-import { getCurrentTraveler, getAllTrips } from './dataModel'
+import { getCurrentTraveler, getAllTrips, getPastTrips, getUpcomingTrips } from './dataModel'
 
 /* ~~~~~~~~~~ EVENT LISTENERS ~~~~~~~~~~*/
 window.addEventListener('load', () => {
   Promise.all(fetchPromises)
   .then(() => {
-    getCurrentTraveler(fetchedData.travelers, 25)
+    console.log('CURRENT TRAVELR', getCurrentTraveler(fetchedData.travelers, 25))
+    console.log('ALL TRIPS', getAllTrips(fetchedData.trips, 25, 'approved'))
+    console.log('PAST TRIPS', getPastTrips(fetchedData.trips, 25))
+    console.log('UPCOMING TRIPS', getUpcomingTrips(fetchedData.trips, 25))
   });
 });
-
