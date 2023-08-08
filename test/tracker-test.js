ab65e5712f0b83269918823b6eecaf13c4c21504
Jan McSorley <mcsorleyjan@gmail.com>
2023-08-07 23:16:33 -0500

    feat: Modify test file

--- test/tracker-test.js
+++ test/tracker-test.js
@@ -1,8 +1,15 @@
 import chai from 'chai';
 const expect = chai.expect;
+import {
+  getCurrentTraveler,
+  setTravelerData,
+  getTravelerData,
+  getTravelerInputs,
+  getAllTrips,
+} from './dataModel';
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
+describe('See if the tests are running', function () {
+  it('should return true', function () {
     expect(true).to.equal(true);
   });
 });
